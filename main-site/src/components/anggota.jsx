2f779044a66eb949d795487955d28fc979024d94
Joseph Simbolon <119671425+JSKUN2@users.noreply.github.com>
2025-10-12 21:38:54 +0700

    Update anggota.jsx

--- main-site/src/components/anggota.jsx
+++ main-site/src/components/anggota.jsx
@@ -14,7 +14,7 @@ const anggota2 = () => {
       jabatan: "Unity Developer",
       keterangan:
         "Boltbert brings the game to life—literally. As the Unity developer, he transforms design and code into an interactive experience that players can touch, see, and feel. He crafts the gameplay mechanics, visual effects, and player controls that define how the world behaves and responds. His expertise turns ideas into a fully playable reality.",
-      link: "https://boltbert.grooths.com/",
+      link: "https://grooths.com/boltbert",
       profil: BoltbertImg,
     },
     {
@@ -22,7 +22,7 @@ const anggota2 = () => {
       jabatan: "Designer",
       keterangan:
         "Glenndovin is the visionary who shapes the world’s aesthetics. From UI layout to environment concept art, he ensures that everything not only looks beautiful but also feels intuitive. His designs create the first impression and the lasting emotional tone of the game, guiding players visually and emotionally through the experience.",
-      link: "https://gln.grooths.com/",
+      link: "https://grooths.com/gln",
       profil: GlendovinnImg,
     },
     {
@@ -119,3 +119,4 @@ export const Anggota = () => {
     </div>
   );
 };
+
