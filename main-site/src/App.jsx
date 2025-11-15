06ddd4193aa0891cbb065346d9c1ba4965495cd8
GroothStudio <studiogrooth@gmail.com>
2025-11-15 07:50:22 +0700

    Add files via upload

--- main-site/src/App.jsx
+++ main-site/src/App.jsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { GroothStudioIntro } from "./components/groothStudioIntro";
 import { Donator } from "./components/donator";
 import { Anggota } from "./components/anggota";
@@ -7,14 +8,50 @@ import { Games } from "./components/games";
 import { FAQ } from "./components/FAQ";
 
 function App() {
+  const sectionsRef = {
+    Home: useRef(null),
+    News: useRef(null),
+    "Our Member": useRef(null),
+    FAQ: useRef(null),
+    "Our Games": useRef(null),
+    "Our Donator": useRef(null),
+  };
+
+  const scrollToSection = (name) => {
+    const section = sectionsRef[name]?.current;
+    if (section) {
+      section.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  };
+
   return (
     <div className="flex items-center justify-center font-poppins flex-col">
-      <GroothStudioIntro />
-      <News />
-      <Anggota />
-      <FAQ />
-      <Games />
-      <Donator />
+      <div ref={sectionsRef["Home"]}>
+        <GroothStudioIntro />
+      </div>
+
+      <div ref={sectionsRef["News"]}>
+        <News />
+      </div>
+
+      <div ref={sectionsRef["Our Member"]}>
+        <Anggota />
+      </div>
+
+      <div ref={sectionsRef["FAQ"]}>
+        <FAQ />
+      </div>
+
+      <div ref={sectionsRef["Our Games"]}>
+        <Games />
+      </div>
+
+      <div ref={sectionsRef["Our Donator"]}>
+        <Donator scrollToSection={scrollToSection} />
+      </div>
     </div>
   );
 }
